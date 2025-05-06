86705cb85c6fd0ab186b488df16aa24840bffdd0
FakeyeT <fakeyetami+github@gmail.com>
2025-05-06 16:52:06 +0100

    fix: build error due to type error

--- src/app/api/tmdb/movie/[id]/route.ts
+++ src/app/api/tmdb/movie/[id]/route.ts
@@ -1,21 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
-import { TmdbIdParams } from "@/types/tmdb";
-
 export async function GET(
   request: NextRequest,
-  { params }: { params: TmdbIdParams },
+  context: { params: { id: string } },
 ) {
-  const { id } = params;
+  const { id } = context.params;
 
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}`,
   );
 
-  if (!res.ok)
+  if (!res.ok) {
     return NextResponse.json(
       { error: "Movie does not exist" },
       { status: 400 },
     );
+  }
 
   const data = await res.json();
   return NextResponse.json(data);
