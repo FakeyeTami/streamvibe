b4c8032337e3210e9f439e57be135abe6835a195
FakeyeT <fakeyetami+github@gmail.com>
2025-05-07 12:24:39 +0100

    fix: updated movie/id/route

--- src/app/api/tmdb/movie/[id]/route.ts
+++ src/app/api/tmdb/movie/[id]/route.ts
@@ -1,21 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
+import { TmdbIdParams } from "@/types/tmdb";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } },
-) {
+export async function GET(request: NextRequest, { params }: TmdbIdParams) {
   const { id } = params;
 
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
   );
 
-  if (!res.ok) {
+  if (!res.ok)
     return NextResponse.json(
       { error: "Movie does not exist" },
       { status: 404 },
     );
-  }
 
   const data = await res.json();
   return NextResponse.json(data);
