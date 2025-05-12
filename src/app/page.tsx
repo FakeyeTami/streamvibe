d229f4a291349c7bd904635d6b563c5c992b136e
FakeyeT <fakeyetami+github@gmail.com>
2025-05-12 15:11:58 +0100

    refactor: updated home page and next.config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,40 +23,52 @@ export default function Home() {
   }, [data]);
 
   return (
-    <section className="relative w-full h-[700px] md:h-[850px] px-4 md:px-6">
-      <div className="absolute top-0 left-0 w-full h-full z-0">
-        {background && (
-          <Image
-            src={background}
-            alt="Trending Movie Background"
-            fill
-            className="object-cover"
-            priority
-          />
-        )}
-        <div className="absolute inset-0 z-10 pointer-events-none bg-[radial-gradient(ellipse_at_center,_rgba(0,0,0,0.3)_0%,_rgba(0,0,0,0.85)_100%)]" />
+    <section className="relative w-full h-[700px] md:h-[850px] px-4 md:px-6 flex items-center justify-center">
+      <div>
+        <div className="absolute inset-0 z-0">
+          <div>
+            {background && (
+              <Image
+                src={background}
+                alt="Trending Movies"
+                fill
+                className="object-cover"
+                priority
+              />
+            )}
+
+            <div className="absolute inset-0 z-10 pointer-events-none bg-[radial-gradient(ellipse_at_center,_rgba(0,0,0,0.3)_0%,_rgba(0,0,0,0.85)_100%)]" />
+          </div>
+          <div className="absolute inset-0 z-20 flex flex-col items-center justify-center">
+            <Image
+              src="/assets/hero-logo.svg"
+              alt="StreamVibe Logo"
+              width={256}
+              height={256}
+              className="object-contain sm:w-2xs sm:h-2xs z-20"
+              aria-hidden={true}
+            />
+          </div>
+        </div>
       </div>
-      <div className="relative z-10 max-w-6xl mx-auto flex flex-col items-center justify-center h-full text-center gap-6">
-        <Image
-          src="/assets/hero-logo.svg"
-          alt="StreamVibe Logo"
-          width={256}
-          height={256}
-          className="object-contain sm:w-2xs sm:h-2xs"
-          aria-hidden="true"
-        />
-        <h1 className="text-3xl md:text-6xl font-semibold text-white">
-          The Best Streaming Experience
-        </h1>
-        <p className="font-light text-sm md:text-md text-white max-w-2xl">
-          StreamVibe is the best streaming experience for watching your favorite
-          movies and shows on demand, anytime, anywhere. With StreamVibe, you
-          can enjoy a wide variety of content, including the latest
-          blockbusters, classic movies, popular TV shows, and more.
-        </p>
-        <Button className="bg-primary-900 font-semibold text-md text-white rounded-lg lg:rounded-xl flex px-3 py-2 md:px-4 md:py-3 gap-3 items-center justify-center">
-          <PlayIcon fill="white" size={15} />
-          Start Watching Now
+      <div className="absolute z-30 -bottom-48 max-w-6xl flex flex-col items-center justify-between gap-12">
+        <div className="flex flex-col items-center justify-between gap-3.5">
+          <h2 className="font-bold text-center text-5xl text-white">
+            The Best Streaming Experience
+          </h2>
+          <p className="text-sm text-center md:text-base">
+            StreamVibe is the best streaming experience for watching your
+            favorite movies and shows on demand, anytime, anywhere. With
+            StreamVibe, you can enjoy a wide variety of content, including the
+            latest blockbusters, classic movies, popular TV shows, and more. You
+            can also create your own watchlist, so you can easily find the
+            content you want to watch.
+          </p>
+        </div>
+
+        <Button className="bg-red-700 text-white flex items-center justify-center py-3.5 px-5 rounded-lg">
+          <PlayIcon fill="white" />
+          <span className="text-sm font-semibold ml-2">Start Watching Now</span>
         </Button>
       </div>
     </section>
